a556ecccb968c4189f1f3c9e37c2ad5c9e8daa2f
lakiert <aleksandra.lakiert@gmail.com>
2022-01-28 00:02:01 +0100

    lil fix

--- zad1/src/com/company/Main.java
+++ zad1/src/com/company/Main.java
@@ -1,38 +1,70 @@
 package com.company;
+import java.math.BigInteger;
+import java.time.LocalDate;
 
-
-interface IntSequence{
-
-    void of(int... x);
-
-}
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        IntSequence sequence = new IntSequence() {
-
-            @Override
-            public void of(int... x) {
-                StringBuilder result = new StringBuilder();
-                for (int i=0; i<x.length; i++)
-                {
-                    result.append(x[i]).append(" ");
-                }
-
-                System.out.println(result);
-
-                while (true){
-                    System.out.println("1 ");
-                }
-
-                }
-        };
-
-
-        sequence.of(1,2,3);
-
+    static class ArrayUtil {
+        public static <T extends Comparable<T>> boolean isSorted(T[] array) {
+            for (int i = 0; i < array.length - 1; i++)
+            {
+                if (array[i + 1].compareTo(array[i]) < 0) return false;
+            }
+            return true;
+        }
     }
 
+
+    public static void main(String[] args) {
+
+        BigInteger[] sortedBig = new BigInteger[]
+                {
+                        BigInteger.valueOf(11111),
+                        BigInteger.valueOf(22222),
+                        BigInteger.valueOf(33333),
+                        BigInteger.valueOf(44444)
+                };
+
+        BigInteger[] unsortedBig = new BigInteger[]
+                {
+                        BigInteger.valueOf(22222),
+                        BigInteger.valueOf(11111),
+                        BigInteger.valueOf(44444),
+                        BigInteger.valueOf(33333)
+                };
+
+        Integer[] sortedInts = new Integer[]{5, 7, 94, 901, 901, 901, 5236};
+        Integer[] unsortedInts = new Integer[]{7246, 901, 9, 523};
+        LocalDate[] sortedDates = new LocalDate[]
+                {
+                        LocalDate.of(2020,8,2),
+                        LocalDate.of(2020,10,16),
+                        LocalDate.of(2020,11,21),
+                        LocalDate.of(2021,4,13)
+                };
+
+        LocalDate[] unsortedDates = new LocalDate[]
+                {
+                        LocalDate.of(2021,8,29),
+                        LocalDate.of(2019,11,1),
+                        LocalDate.of(2020,3,6),
+                        LocalDate.of(2021,12,3)
+                };
+
+        String[] sortedString = new String[]{"aa", "b", "c", "d"};
+        String[] unsortedString = new String[]{"d", "c", "a", "b"};
+
+
+
+        System.out.println(ArrayUtil.isSorted(sortedInts));
+        System.out.println(ArrayUtil.isSorted(unsortedInts));
+        System.out.println(ArrayUtil.isSorted(sortedDates));
+        System.out.println(ArrayUtil.isSorted(unsortedDates));
+        System.out.println(ArrayUtil.isSorted(sortedBig));
+        System.out.println(ArrayUtil.isSorted(unsortedBig));
+        System.out.println(ArrayUtil.isSorted(sortedString));
+        System.out.println(ArrayUtil.isSorted(unsortedString));
+
+    }
 }
