9aff3f81953cc19b919756c388346d523f2a1136
lakiert <aleksandra.lakiert@gmail.com>
2022-01-28 18:30:25 +0100

    added zad3

--- zad3/src/com/company/Main.java
+++ zad3/src/com/company/Main.java
@@ -1,17 +1,40 @@
 package com.company;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+
+interface FileTypeInterface{
+    ArrayList<String> choose(String[] listOfFiles, String type);
+}
+
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-        Comparator<String> stringComparatorLambda =
-                (o1, o2) -> o1.compareTo(o2);
+        File dir = new File("D:\\filefolder");
+        String[] list = dir.list();
 
-        int lambdaComparator = stringComparatorLambda.compare("b", "a");
-        System.out.println(lambdaComparator);
+        FileTypeInterface filter = (listOfFiles, type) -> {
+
+            ArrayList<String> result = new ArrayList<>();
+
+            for (int i=0; i<listOfFiles.length; i++)
+            {
+                if (!listOfFiles[i].toLowerCase().endsWith(type)){
+                    result.add(listOfFiles[i]);
+                }
+            }
+
+            return result;
+
+            };
+
+
+        System.out.println("pliki bez .txt : " + filter.choose(list, ".txt"));
+        System.out.println("pliki bez .png : " + filter.choose(list, ".png"));
 
     }
 }
